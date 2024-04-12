d13d60b30cb195f7451591d1adafe59115bf2ec9
TimLeHamster <79696450+TimLeHamster@users.noreply.github.com>
2024-04-12 16:59:31 +0200

    path parsing added

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -9,8 +9,15 @@ private:
 	std::string _req;
 	std::string _method;
 	std::string _uri;
+	std::string _host;
+	std::size_t _port;
+	std::string _path;
+	std::string _query;
+	std::string _fragment;
 	std::string _version;
 	std::string _body;
+
+	void isValidURI();
 
 public:
 	// Constructors
@@ -18,7 +25,6 @@ public:
 	~Request();
 
 	void Answer();
-	bool isValidURI(const std::string &uri);
 };
 
 #endif
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <algorithm>
 
-Request::Request(std::string &msg)
+Request::Request(std::string &msg) : _req(msg), _method("GET"), _uri("http://localhost:80"), _host("localhost"), _port(80), _path(""), _query(""), _fragment(""), _version("1.1"), _body("")
 {
 	try
 	{
@@ -35,7 +35,7 @@ Request::Request(std::string &msg)
 
 		if (this->_method != "GET" && this->_method != "POST" && this->_method != "DELETE")
 			throw std::runtime_error("Invalid method: " + this->_method);
-		isValidURI(this->_uri);
+		isValidURI();
 		if (this->_version != "HTTP/1.1\r")
 			throw std::runtime_error("Invalid version: " + this->_version);
 		// Add all the parameters parsing
@@ -59,35 +59,61 @@ void Request::Answer()
 	std::cout << "Version: " << this->_version << std::endl;
 }
 
-bool Request::isValidURI(const std::string &uri)
+void Request::isValidURI()
 {
-
 	std::string authority;
 
-	if (uri.empty() ||
-		uri.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~:/?#[]@!$&'()*+,;=%") != std::string::npos)
-		return false;
+	if (this->_uri.empty() ||
+		this->_uri.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~:/?#[]@!$&'()*+,;=%") != std::string::npos)
+		throw std::runtime_error("Invalid URI: " + this->_uri);
 
 	// Scheme verification (maybe add a behavior to allow no-scheme URIs like NGINX in which it is http:// by default)
-	if (uri.substr(0, 5) != "http:")
-		return false;
+	if (this->_uri.substr(0, 5) != "http:")
+		throw std::runtime_error("Invalid URI: " + this->_uri);
 
 	// Authority parsing
-	std::size_t authorityStart = uri.find("//");
+	std::size_t authorityStart = this->_uri.find("//");
 	if (authorityStart != std::string::npos)
 	{
-		std::size_t authorityEnd = uri.find_first_of("/?#", authorityStart + 2);
-		authority = uri.substr(authorityStart + 2, authorityEnd - authorityStart - 2);
+		std::size_t authorityEnd = this->_uri.find_first_of("/?#", authorityStart + 2);
+		authority = this->_uri.substr(authorityStart + 2, authorityEnd - authorityStart - 2);
 		std::size_t portStart = authority.find(':');
 		if (portStart != std::string::npos && portStart != authority.length() - 1)
 		{
 			std::string port = authority.substr(portStart + 1);
 			if (port.find_first_not_of("0123456789") != std::string::npos)
-				return false;
+				throw std::runtime_error("Invalid URI: " + this->_uri);
+			this->_port = std::stoi(port);
 		}
 	}
 
 	// Path parsing
-
-	return true;
+	if (authorityStart == std::string::npos)
+		authorityStart = 5;
+	std::size_t pathStart = this->_uri.find('/', authorityStart + 2);
+	if (pathStart != std::string::npos)
+	{
+		std::size_t queryStart = this->_uri.find('?', pathStart + 1);
+		std::size_t fragmentStart = this->_uri.find('#', pathStart + 1);
+		if (queryStart != std::string::npos)
+		{
+			this->_path = this->_uri.substr(pathStart, queryStart - pathStart);
+			if (fragmentStart != std::string::npos)
+			{
+				this->_query = this->_uri.substr(queryStart, fragmentStart - queryStart);
+				this->_fragment = this->_uri.substr(fragmentStart);
+			}
+			else
+				this->_query = this->_uri.substr(queryStart);
+		}
+		else if (fragmentStart != std::string::npos)
+		{
+			this->_path = this->_uri.substr(pathStart, fragmentStart - pathStart);
+			this->_fragment = this->_uri.substr(fragmentStart);
+		}
+		else
+			this->_path = this->_uri.substr(pathStart);
+	}
+	else
+		this->_path = "/";
 }
